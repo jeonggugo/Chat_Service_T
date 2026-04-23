365f4d940e177dfcd4e083a5acf2f5101969c846
jeongkuk <herojk5019@naver.com>
2026-04-23 17:02:17 +0900

    순수 WebSocket 백엔드 적용- 간단구현

--- src/main/java/com/example/chatserver/chat/config/WebSocketConfig.java
+++ src/main/java/com/example/chatserver/chat/config/WebSocketConfig.java
@@ -1,4 +1,25 @@
 package com.example.chatserver.chat.config;
 
-public class WebSocketConfig {
+import org.springframework.context.annotation.Configuration;
+import org.springframework.web.socket.config.annotation.EnableWebSocket;
+import org.springframework.web.socket.config.annotation.WebSocketConfigurer;
+import org.springframework.web.socket.config.annotation.WebSocketHandlerRegistry;
+
+@Configuration
+@EnableWebSocket
+public class WebSocketConfig implements WebSocketConfigurer {
+    private final SimpleWebSocketHandler simpleWebSocketHandler;
+
+    public WebSocketConfig(SimpleWebSocketHandler simpleWebSocketHandler) {
+        this.simpleWebSocketHandler = simpleWebSocketHandler;
+    }
+
+    @Override
+    public void registerWebSocketHandlers(WebSocketHandlerRegistry registry) {
+//        /connect url로 websocket 연결 요청이 들어오면, 핸들러 클래스가 처
+        registry.addHandler(simpleWebSocketHandler, "/connect")
+//        securityconfig에서의 corsr예외는 http요청에 대한 예외. 따라서 websocket 프로토콜에 대한 요청에 대해서는 별도의 cors설정 필요하다.
+
+            .setAllowedOrigins("http://localhost:3000");
+    }
 }
