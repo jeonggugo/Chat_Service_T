092dde0206dca35effee0a6fa51d1349724a3ab2
jeongkuk <herojk5019@naver.com>
2026-04-24 17:21:33 +0900

    Feat: STOMP EventListner을 통해 connect와 disconnect를 시각화 Fix: sendTo를 이용한 간단한 메세지 매핑 방식에서 MessageTemplate를 사용하여 확장성을 넓도록 방식변경

--- src/main/java/com/example/chatserver/chat/controller/StompController.java
+++ src/main/java/com/example/chatserver/chat/controller/StompController.java
@@ -1,19 +1,42 @@
 package com.example.chatserver.chat.controller;
 
+import com.example.chatserver.chat.dto.ChatMessageReqDto;
+import lombok.extern.slf4j.Slf4j;
 import org.springframework.messaging.handler.annotation.DestinationVariable;
 import org.springframework.messaging.handler.annotation.MessageMapping;
 import org.springframework.messaging.handler.annotation.SendTo;
+import org.springframework.messaging.simp.SimpMessageSendingOperations;
 import org.springframework.stereotype.Controller;
 
+@Slf4j
 @Controller
 public class StompController {
+    private final SimpMessageSendingOperations messageTemplate;
 
-    @MessageMapping("/{roomId}") // 클라이언트에서 특정 publish/roomId 형태로 메시지를 발행시 MessageMapping 수신
-    @SendTo("/topic/{roomId}") //해당 roomId에 메시지를 발행하여 구독중인 클라이언트에게 메시지 전송
-//    DestinationVariable은 @MessageMapping 어노테이션으로 정의된 Websocket Controller 내에서만 사용한다.
-    public String sendMessage(@DestinationVariable Long roomId, String message){
-        System.out.println(message);
-        return message;
+    public StompController(SimpMessageSendingOperations messageTemplate) {
+        this.messageTemplate = messageTemplate;
     }
 
+//    방법1. MessageMapping(수신)과 SendTo(topic에 메시지전달)한번에 처리
+
+//    @MessageMapping("/{roomId}") // 클라이언트에서 특정 publish/roomId 형태로 메시지를 발행시 MessageMapping 수신
+//    @SendTo("/topic/{roomId}") //해당 roomId에 메시지를 발행하여 구독중인 클라이언트에게 메시지 전송
+
+    /// /    DestinationVariable은 @MessageMapping 어노테이션으로 정의된 Websocket Controller 내에서만 사용한다.
+//    public String sendMessage(@DestinationVariable Long roomId, String message){
+//        System.out.println(message);
+//        return message;
+//    }
+
+
+
+
+//  방법2. MessageMapping 어노테이션만 활용.
+
+    @MessageMapping("/{roomId}") // 클라이언트에서 특정 publish/roomId 형태로 메시지를 발행시 MessageMapping 수신
+//    DestinationVariable은 @MessageMapping 어노테이션으로 정의된 Websocket Controller 내에서만 사용한다.
+    public void sendMessage(@DestinationVariable Long roomId, ChatMessageReqDto chatMessageReqDto) {
+        log.info(chatMessageReqDto.getSenderEmail()+"님의 메시지: "+chatMessageReqDto.getMessage());
+        messageTemplate.convertAndSend("/topic/"+roomId, chatMessageReqDto);
+    }
 }
