e21e854ea8a9d8a0e911fad24e4745400368b5b2
jeongkuk <herojk5019@naver.com>
2026-04-24 12:01:27 +0900

    Feat: STOMP 적용 및 JWT 토큰을 이용한 connect검증

--- src/main/java/com/example/chatserver/chat/config/SimpleWebSocketHandler.java
+++ src/main/java/com/example/chatserver/chat/config/SimpleWebSocketHandler.java
@@ -1,41 +1,41 @@
-package com.example.chatserver.chat.config;
-
-import java.util.HashSet;
-import java.util.Set;
-import java.util.concurrent.ConcurrentHashMap;
-import lombok.extern.slf4j.Slf4j;
-import org.springframework.stereotype.Component;
-import org.springframework.web.socket.BinaryMessage;
-import org.springframework.web.socket.CloseStatus;
-import org.springframework.web.socket.PongMessage;
-import org.springframework.web.socket.TextMessage;
-import org.springframework.web.socket.WebSocketMessage;
-import org.springframework.web.socket.WebSocketSession;
-import org.springframework.web.socket.handler.TextWebSocketHandler;
-//connect로 웹소켓 연결요청이 들어왔을 때 이를 처리할 클래스이다.
-@Component
-@Slf4j
-public class SimpleWebSocketHandler extends TextWebSocketHandler {
-    private final Set<WebSocketSession> sessions = ConcurrentHashMap.newKeySet();
-    @Override
-    public void afterConnectionEstablished(WebSocketSession session) throws Exception {
-        sessions.add(session);
-        log.info("Connected :"+session.getId());
-    }
-
-    protected void handleTextMessage(WebSocketSession session, TextMessage message) throws Exception {
-        String payload = message.getPayload();
-        log.info("received message: "+payload);
-        for (WebSocketSession s : sessions) {
-            if(s.isOpen()){
-                s.sendMessage(new TextMessage(payload));
-            }
-        }
-    }
-
-    @Override
-    public void afterConnectionClosed(WebSocketSession session, CloseStatus status) throws Exception {
-        sessions.remove(session);
-        log.info("disconnected");
-    }
-}
+//package com.example.chatserver.chat.config;
+//
+//import java.util.HashSet;
+//import java.util.Set;
+//import java.util.concurrent.ConcurrentHashMap;
+//import lombok.extern.slf4j.Slf4j;
+//import org.springframework.stereotype.Component;
+//import org.springframework.web.socket.BinaryMessage;
+//import org.springframework.web.socket.CloseStatus;
+//import org.springframework.web.socket.PongMessage;
+//import org.springframework.web.socket.TextMessage;
+//import org.springframework.web.socket.WebSocketMessage;
+//import org.springframework.web.socket.WebSocketSession;
+//import org.springframework.web.socket.handler.TextWebSocketHandler;
+////connect로 웹소켓 연결요청이 들어왔을 때 이를 처리할 클래스이다.
+//@Component
+//@Slf4j
+//public class SimpleWebSocketHandler extends TextWebSocketHandler {
+//    private final Set<WebSocketSession> sessions = ConcurrentHashMap.newKeySet();
+//    @Override
+//    public void afterConnectionEstablished(WebSocketSession session) throws Exception {
+//        sessions.add(session);
+//        log.info("Connected :"+session.getId());
+//    }
+//
+//    protected void handleTextMessage(WebSocketSession session, TextMessage message) throws Exception {
+//        String payload = message.getPayload();
+//        log.info("received message: "+payload);
+//        for (WebSocketSession s : sessions) {
+//            if(s.isOpen()){
+//                s.sendMessage(new TextMessage(payload));
+//            }
+//        }
+//    }
+//
+//    @Override
+//    public void afterConnectionClosed(WebSocketSession session, CloseStatus status) throws Exception {
+//        sessions.remove(session);
+//        log.info("disconnected");
+//    }
+//}
